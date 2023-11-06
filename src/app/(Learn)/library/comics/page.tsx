2dc8b4e31dde6ba12bdcb2bdaf5fbf49db35446a
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-11-06 14:12:23 +0530

    comic page structural changes

--- src/app/(Learn)/library/comics/page.tsx
+++ src/app/(Learn)/library/comics/page.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import Card from "@/blocks/UI/Card";
-import { BiLockAlt, BiShare } from "react-icons/bi";
-import SharePopup from "@/blocks/atoms/SharePopup";
+import { BiLockAlt } from "react-icons/bi";
 import BuyPopup from "@/blocks/atoms/BuyPopup";
+import SharePopup from "@/blocks/atoms/SharePopup";
 
 const Page = () => {
   return (
@@ -30,17 +31,7 @@ const Page = () => {
         </div>
       </Card>
 
-      <Card title="Book Details" className="mt-5 mb-10">
-        <p>
-          From tea-serving robots in feudal Japan to modern rovers exploring
-          Mars, robots have been humanitys partners, helpers, and protectors for
-          centuries! Join one of the worlds earliest robots, a mechanical bird
-          named Pouli, as he explores where robots came from, how they work, and
-          where they’re going in this informative and hilarious new book! Ever
-          dreamt of building your own best friend? It might be easier than you
-          think!
-        </p>
-      </Card>
+      <Reward />
     </div>
   );
 };
@@ -72,11 +63,11 @@ const Hero = () => {
           </h1>
           <div className="grid grid-cols-3  my-5">
             <Infor title="Published:" value="November 02, 2023" />
-            <Infor title="Penciler:" value="Marcio Fiorito" />
+            <Infor title="Creator:" value="Deepak Vishwakarma" />
           </div>
           <div className="my-5 grid grid-cols-3">
-            <Infor title="Cover Artist:" value="Marcio Fiorito" />
-            <Infor title="Total Pages:" value="23" />
+            <Infor title="Credits Required:" value="1,000 CRD" />
+            <Infor title="Page Count:" value="23" />
             <Infor title="Grade:" value="6th" />
           </div>
 
@@ -88,15 +79,67 @@ const Hero = () => {
 
           <div className="mt-10  bg-white p-5 rounded-xl">
             <section className="flex gap-5 items-center ">
-              <h1 className="text-3xl font-semibold font-game mr-5">
+              <h1 className="text-3xl font-semibold font-game mr-2">
                 400.99 <span className="text-sm">CRD</span>
               </h1>
               <BuyPopup />
               <SharePopup />
             </section>
           </div>
+
+          <div>
+            <Quiz />
+          </div>
         </div>
       </div>
     </div>
+  );
+};
+
+const Quiz = () => {
+  return (
+    <div className="bg-white mt-5 flex gap-5 p-10 rounded-xl items-cener justify-between">
+      <div>
+        <h3 className="font-heading text-xl font-semibold">
+          Complete Quiz & Earn Reward
+        </h3>
+        <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <button className=" py-3 px-10 flex items-center justify-center text-white  bg-black rounded-full font-heading  gap-2">
+        <BiLockAlt /> Unlock
+      </button>
+    </div>
+  );
+};
+
+const Reward = () => {
+  return (
+    <Card title="Rewards" className="mt-5">
+      <div className=" rounded-xl grid grid-cols-4  gap-5">
+        <div className="rounded-xl center flex-col">
+          <Image
+            src="/cup.jpg"
+            width={200}
+            height={200}
+            alt="price"
+            className="rounded-xl"
+          />
+          <p className="font-heading text-lg mt-2 font-medium">100 CRDs</p>
+          <p className="text-gray-500">After Comics completion</p>
+        </div>
+        <div className="rounded-xl center flex-col">
+          <Image
+            src="/cup.jpg"
+            width={200}
+            height={200}
+            alt="price"
+            className="rounded-xl"
+          />
+          <p className="font-heading text-lg mt-2 font-medium">100 CRDs</p>
+          <p className="text-gray-500">After Quiz completion</p>
+        </div>
+      </div>
+    </Card>
   );
 };
