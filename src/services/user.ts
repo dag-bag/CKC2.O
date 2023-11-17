211dff2e0452a40aa87a95680b2474ec4f53cabd
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-17 22:48:02 +0530

    Updated Backend

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,13 +3,20 @@ const getUser = async (accessToken: string) => {
   try {
     const { user, jwt } = await strapi.authenticateProvider(
       "google",
-      accessToken
+      "ya29.a0AfB_byAZi6nMmNzysqMJgm8t8UCoNsdVbhiGTYo1I-2P1ASe8gH9XLQigEkPe8a958_4HtcViNIBCQxBjpIMxh535mW606wkmTlCYbsEqsyEvqeN4bs8eJzDe79q8h_I_q_e70dsyn-e3Wc5i11j8sKPww0yi3pfIEYaCgYKAU4SARESFQHGX2Mi4CkTiGddJXKx6AYfqa747w0170"
     );
-    console.log(accessToken);
     return { user, jwt };
   } catch (error) {
-    throw new Error("Something Went wrong");
+    console.log(error);
   }
 };
 
-export { getUser };
+const updateUser = async (data: any) => {
+  const res = await strapi.update("users", 1, data);
+  return res.data;
+};
+const daleteUser = async (id: number) => {
+  const res = await strapi.delete("users", id);
+  return res.data;
+};
+export { getUser, updateUser, daleteUser };
