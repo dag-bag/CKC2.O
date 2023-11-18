fa7fde651164f4e407675efb8f1945ab03157c1a
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-11-18 16:08:11 +0530

    log after every 30 seconds

--- src/blocks/temporary/Video.tsx
+++ src/blocks/temporary/Video.tsx
@@ -2,18 +2,22 @@
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 
-const VideoPlayer = ({ userId, videoId, videoDuration }) => {
+const VideoPlayer = ({ userId, videoId, videoDuration }: any) => {
   const [played, setPlayed] = useState(0);
 
-  const handleProgress = (progress) => {
-    console.log("progress", progress);
+  const handleProgress = (progress: any) => {
+    const roundedPlayedSeconds = Math.round(progress.playedSeconds);
 
-    setPlayed(progress.played);
+    if (roundedPlayedSeconds !== 0) {
+      if (roundedPlayedSeconds % 30 === 0) {
+        console.log(`>> fetch it, ${roundedPlayedSeconds} seconds played!`);
+      }
+    }
   };
 
   const handleEnded = () => {
     // Call the API to update progress or trigger rewards when the video is completed
-    updateVideoProgress(userId, videoId, videoDuration);
+    // updateVideoProgress(userId, videoId, videoDuration);
   };
 
   return (
