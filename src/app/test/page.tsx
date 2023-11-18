52c7ebf38ffb0bdb91af4c9dc584c616601c8921
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-11-18 17:10:42 +0530

    pp

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,18 +1,10 @@
 import VideoPlayer from "@/blocks/temporary/Video";
 
 const WatchPage = () => {
-  const userId = "user123"; // Replace with your authentication logic
-  const videoId = "video456"; // Replace with the current video being watched
-  const videoDuration = 300; // Replace with the actual duration of the video in seconds
-
   return (
     <div>
       <h1>Watch Video</h1>
-      <VideoPlayer
-        userId={userId}
-        videoId={videoId}
-        videoDuration={videoDuration}
-      />
+      <VideoPlayer />
     </div>
   );
 };
--- src/blocks/temporary/Video.tsx
+++ src/blocks/temporary/Video.tsx
@@ -2,12 +2,26 @@
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 
-const paylaod = {
-  content_id: "random123",
-  user_id: "random345",
+// this is for testing data;
+
+const purchase = {
+  id: "random123",
 };
 
-const VideoPlayer = ({ userId, videoId, videoDuration }: any) => {
+const content = {
+  id: "random123",
+};
+
+const user = {
+  id: "random345",
+};
+
+const reward = {
+  type: "coin",
+  value: 100,
+};
+
+const VideoPlayer = () => {
   const [played, setPlayed] = useState(0);
 
   const handleProgress = (progress: any) => {
@@ -21,14 +35,14 @@ const VideoPlayer = ({ userId, videoId, videoDuration }: any) => {
   };
 
   const handleEnded = () => {
-    // Call the API to update progress or trigger rewards when the video is completed
-    // updateVideoProgress(userId, videoId, videoDuration);
+    //  at this section we need to
   };
 
   return (
     <ReactPlayer
       url="https://vimeo.com/883775791"
       controls
+      playsinline
       onProgress={handleProgress}
       onEnded={handleEnded}
     />
@@ -36,3 +50,8 @@ const VideoPlayer = ({ userId, videoId, videoDuration }: any) => {
 };
 
 export default VideoPlayer;
+
+// at StartPlaying - we need to make a table which need content_id and user_id and storing and keep updating progress every 30 seconds
+// at onEnded - take reward from content and provide reward to user_id
+
+// now no need to store reward at watched table
