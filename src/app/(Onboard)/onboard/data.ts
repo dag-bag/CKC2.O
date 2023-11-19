30abbda27f0a684b09b13060afd90bc12b936f6d
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-11-19 10:52:59 +0530

    update score system

--- src/app/(Onboard)/onboard/data.ts
+++ src/app/(Onboard)/onboard/data.ts
@@ -2,37 +2,32 @@ const Stage = {
   "/onboard/name": {
     question: "Hello there, Cosmic Kid! 😊 What's your super cool name?",
     next: "/onboard/standard",
-    score: 16,
+    score: 14,
   },
   "/onboard/standard": {
     question: "Awesome! 🚀 Which grade are you in, young explorer?",
     next: "/onboard/birthday",
-    score: 32,
+    score: 28,
   },
   "/onboard/birthday": {
     question: "Wow, that's exciting! 🎉 When's your special birthday?",
     next: "/onboard/phone",
-    score: 48,
+    score: 42,
   },
   "/onboard/phone": {
     question: "Just in case we need to reach you in the Cosmic galaxy 📞, what's your phone number?",
     next: "/onboard/location",
-    score: 80,
+    score: 46,
   },
   "/onboard/location": {
     question: "We'd like to know more about your location in the Cosmic galaxy!  🌍✨ ",
     next: "/onboard/profile ",
-    score: 80,
-  },
-  "/onboard/plan": {
-    question: "Time to choose your Cosmic adventure! 🌌 Which plan do you think will make you a superhero?",
-    next: "/onboard/profile",
-    score: 80,
+    score: 70,
   },
   "/onboard/profile": {
     question: "You're almost ready to soar through the stars! 🌟 How about creating your own out-of-this-world avatar?",
     next: "/onboard/complete",
-    score: 96,
+    score: 84,
   },
   "/onboard/complete": {
     question: "Yay! your profile is completed successfully. Oh you got amazing rewards!.",
