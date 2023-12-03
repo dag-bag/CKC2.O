e8d29a8a82dcfa78558b613fd8944b8e32c652b1
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-12-03 23:37:25 +0530

    1st phase

--- src/blocks/molecules/player/index.tsx
+++ src/blocks/molecules/player/index.tsx
@@ -3,7 +3,7 @@ import Loader from "./loader";
 import ReactPlayer from "react-player";
 import { useState, useRef } from "react";
 import useVideoPlayer from "@/hooks/useVideo";
-
+import toast from "react-hot-toast";
 import { createReward } from "@/strapi/services/custom";
 
 interface Props {
@@ -51,25 +51,61 @@ const Player: React.FC<Props> = ({
         customToast();
       }
     );
-
-    // Check if the video has reached 80% completion
-    // const duration = playerRef.current.getDuration();
-    // const progress = (watchRecords.at(0)?.watch_progress ?? 0) / duration;
-    // if (progress >= 0.8) {
-    //   // Handle the end of the video at 80% completion
-    //   console.log("Video reached 80% completion!");
-    // }
   };
+
+  const forceUpdate = (value: number) => {
+    console.log("force update << ");
+    if (playerRef.current) {
+      setPlaying(false);
+      playerRef.current.seekTo(value, "seconds");
+      setPlaying(true);
+    }
+  };
+
+  const seconds = useRef<any>([]);
+  const [playing, setPlaying] = useState(false);
+
+  const progress = (state: any) => {
+    // saving values
+    seconds.current.push(Math.round(state.playedSeconds));
+    // emptying array
+    seconds.current.length > 3 && seconds.current.shift();
+    // seperating
+    const zones = {
+      previous: seconds.current.at(seconds.current.length - 2) ?? 0,
+      current: seconds.current.at(seconds.current.length - 1),
+    };
+
+    console.log(zones);
+
+    // status
+    const signal =
+      zones.previous == 0
+        ? "green"
+        : zones.current - zones.previous > 10 ||
+          zones.current - zones.previous == 0
+        ? "green"
+        : "red";
+
+    // action
+    if (signal == "red") {
+      toast("you cannot seek");
+      forceUpdate(zones.previous);
+      seconds.current = [0, 0];
+    }
+  };
+
   return (
     <div className="w-full">
       <ReactPlayer
+        playing={playing}
         controls
         playsinline
         url={mediaURL}
         width={"100%"}
         ref={playerRef}
         height={"100%"}
-        onProgress={handleProgress}
+        onProgress={progress}
         onReady={handleReadyToWatch}
         onEnded={handleVideoEnd}
       />
