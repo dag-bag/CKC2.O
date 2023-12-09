10fceca3feec2785a94d05f7faa4631df6fbcd95
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-12-09 13:40:33 +0530

    Discovery Jar Done

---
+++ src/hooks/useCourse.ts
@@ -0,0 +1,100 @@
+// useVideoPlayer.tsx
+import useSWR, { useSWRConfig } from "swr";
+import { strapi } from "@/libs/strapi";
+import toast from "react-hot-toast";
+
+interface VideoPlayerProps {
+  userId: string;
+  contentId: string;
+  contentType: string;
+}
+
+type WatchRecord = {
+  id: string;
+  user_id: string;
+  content_id: string;
+  watched_date: string;
+  type: string;
+  watch_progress: number;
+};
+
+type VideoPlayerResult = {
+  trackProgress: (progress: { playedSeconds: number }) => Promise<void>;
+  isLoading: boolean;
+  lastPlayed: number;
+};
+
+const useCourse = ({
+  userId,
+  contentId,
+  contentType,
+}: VideoPlayerProps): VideoPlayerResult => {
+  const { mutate } = useSWRConfig();
+
+  const getWatchedRecords = async (): Promise<WatchRecord[]> => {
+    try {
+      const response = await strapi.find("watcheds", {
+        filters: {
+          content_id: contentId,
+          user_id: userId,
+        },
+      });
+      return response.data as WatchRecord[];
+    } catch (error) {
+      return [];
+    }
+  };
+
+  const createWatchRecord = async (): Promise<void> => {
+    await strapi.create("watcheds", {
+      user_id: userId,
+      content_id: contentId,
+      watched_date: new Date().toISOString(),
+      type: contentType,
+      watch_progress: 0,
+    });
+    mutate(`watched/${contentId}`);
+  };
+
+  const updateWatchRecord = async (watchProgress: number): Promise<void> => {
+    const watchRecordToUpdate = watchRecords?.find(
+      (record) => record.content_id === contentId && record.user_id === userId
+    );
+    if (watchRecordToUpdate) {
+      await strapi.update("watcheds", watchRecordToUpdate.id, {
+        watch_progress: watchProgress,
+      });
+    }
+  };
+
+  const { data: watchRecords, isLoading } = useSWR<WatchRecord[]>(
+    `watched/${contentId}`,
+    getWatchedRecords,
+    {
+      onSuccess: (data) => {
+        if (data.length === 0) {
+          createWatchRecord();
+        }
+      },
+    }
+  );
+
+  const trackProgress = async (progress: {
+    playedSeconds: number;
+  }): Promise<void> => {
+    const roundedPlayedSeconds = Math.floor(progress.playedSeconds);
+    if (roundedPlayedSeconds !== 0 && roundedPlayedSeconds % 10 === 0) {
+      console.log("watch recored - ", roundedPlayedSeconds);
+
+      await updateWatchRecord(roundedPlayedSeconds);
+    }
+  };
+
+  return {
+    isLoading,
+    trackProgress,
+    lastPlayed: watchRecords?.at(0)?.watch_progress ?? 0,
+  };
+};
+
+export default useCourse;
--- src/strapi/populations/index.ts
+++ src/strapi/populations/index.ts
@@ -1,3 +1,6 @@
 export const CAROUSEL_P = ["slides"];
 export const CHALLANGE_REQ_P = ["user"];
-export const DISCONVER_JAR_CONFIG_P = ["discovery_jar_answers", "discovery_jar_questions.time_stamps",];
+export const DISCONVER_JAR_CONFIG_P = [
+  "discovery_jar_answers",
+  "discovery_jar_answers.time_stamps",
+];
