9c29eb94f71c60f4b8302896b9ba140bf3fd66ef
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-12-17 02:38:32 +0530

    Update page.tsx

--- src/app/hello/[...segments]/page.tsx
+++ src/app/hello/[...segments]/page.tsx
@@ -11,11 +11,16 @@ interface Props {
 
 const Page: React.FC<Props> = async ({ params: { segments } }) => {
   const [data, purchases] = await Promise.all([
-    Quiz({ type: "GET_ONE", payload: parseInt(segments[1]) }),
+    Quiz({ type: "GET_ONE", payload: parseInt(segments[2]) }),
     getTransactions(segments[0]),
     // getUserRewards(user.user.id),
   ]);
+  const id = segments[1];
+  // @ts-ignore
+  const locked = !purchases
+    .map((pur: any) => pur.content_id)
+    .includes(id.toString());
 
-  return <div>{JSON.stringify({ data: data, purchases })}</div>;
+  return <div>{JSON.stringify({ data: data, purchases, locked })}</div>;
 };
 export default Page;
