c9c84f71cf958794343f8ebbbd8835ef6a4026cc
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-12-16 21:16:04 +0530

    Premium feature added

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+import useSession from "@/hooks/use-session";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  const router = useRouter();
+  const { session } = useSession();
+  const buyPremium = async () => {
+    const data = await axios
+      .post("/api/user/unlock/premium", {
+        plan: 1,
+        title: "Super Premium",
+      })
+      .then((res) => router.refresh());
+    console.log(data);
+  };
+  return (
+    <div onClick={buyPremium}>
+      <button>Buy Premium</button>
+      {JSON.stringify(session)}
+    </div>
+  );
 }
