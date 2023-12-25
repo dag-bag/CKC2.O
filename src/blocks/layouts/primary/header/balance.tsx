4953cdf392ee233376fdc268a36c08e87e9fb1cd
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-12-25 15:11:23 +0530

    Update balance component styling and icons

--- src/blocks/layouts/primary/header/balance.tsx
+++ src/blocks/layouts/primary/header/balance.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import useCoins from "@/hooks/useCoins";
 import Loading from "@/blocks/atoms/loading";
 import useSession from "@/hooks/use-session";
-
 const MyBalance = () => {
   const {
     data: { isLoading, data },
@@ -13,22 +12,16 @@ const MyBalance = () => {
   return (
     <Link
       href="/purchases"
-      className="h-[45px] flex items-center rounded-full px-5 bg-white "
+      className="h-[45px] flex items-center rounded-full px-5 bg-blue-500 text-white "
     >
       <div className="flex gap-8 text-lg">
-        <button className="flex items-center gap-2 font-josefin">
-          <Image width={35} height={35} alt="coin" src={"/coin3.png"} />
-          <span className="mt-1 font-semibold">{data.coins}</span>
+        <button className="flex items-center gap-2 font-heading">
+          <Image width={23} height={23} alt="coin" src={"/assets/credit.png"} />
+          <span className=" font-semibold">{data.coins}</span>
         </button>
-        <button className="flex items-center gap-2 font-josefin">
-          <Image
-            width={30}
-            height={30}
-            alt="coin"
-            src={"/diamond.png"}
-            className=" -rotate-12"
-          />
-          <span className="mt-1 font-semibold">1,000</span>
+        <button className="flex items-center gap-2 font-heading">
+          <Image width={23} height={23} alt="coin" src={"/assets/coins.png"} />
+          <span className="font-semibold">1,000</span>
         </button>
       </div>
     </Link>
