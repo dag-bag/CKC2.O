3f7a5c925a98cc1136bec161bb7a227f063de01a
dag-bag <88625252+dag-bag@users.noreply.github.com>
2024-01-06 15:34:57 +0530

    avatar updated

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import AvatarListing from "@/blocks/molecules/profile/avatars";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <AvatarListing />
+    </div>
+  );
+}
--- src/blocks/molecules/profile/avatars/AvatarBlock.tsx
+++ src/blocks/molecules/profile/avatars/AvatarBlock.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useSession from "@/hooks/use-session";
+import { updateUser } from "@/services/user";
 import Image from "next/image";
 
 interface Props {
@@ -10,11 +11,14 @@ interface Props {
 
 const AvatarBlock: React.FC<Props> = ({ title, images }) => {
   const session = useSession();
-  const handleClickEvent = () => {
+  console.log(session);
+  const handleClickEvent = async () => {
+    await updateUser({ avatar: images });
     console.log("change avatar");
   };
   return (
     <div onClick={handleClickEvent} className="border">
+      {JSON.stringify(session.session)}
       <div className="relative aspect-w-4 aspect-h-4">
         <Image fill alt={title} src={images} />
       </div>
