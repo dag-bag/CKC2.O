ca057b682f6f3d98bb10fd11078b3d2c47067990
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-01-07 13:37:16 +0530

    update

--- src/app/(unauthorize)/auth/register/page.tsx
+++ src/app/(unauthorize)/auth/register/page.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 const Page = () => {
   return (
-    <div className="md:h-screen center font-fun bg-gray-100 ">
+    <div className="md:h-screen center font-fun bg-gray-100">
       <div className="rounded-lg grid md:grid-cols-2 bg-white max-w-7xl w-full  mx-auto ">
         <div className="bg-blue-100 rounded-lg center hidden md:flex">
           <Image
@@ -22,7 +22,7 @@ const Page = () => {
             src={"/onboard/registration.png"}
           />
         </div>
-        <div className="md:p-10 xl:p-20 p-8 ">
+        <div className="md:p-10 xl:p-20 p-8">
           <Heading size="large" className="font-semibold font-amar mb-5">
             Join Cosmic Kids Club <br /> Learning & Adventure!
           </Heading>
