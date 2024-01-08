2c57fe12e0777c6d03cdb2d197425d1333ad0d75
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-01-08 22:04:19 +0530

    refactor images

--- src/libs/extImage.ts
+++ src/libs/extImage.ts
@@ -1,7 +1,7 @@
 const extImage = (thumbnailObj: any) => {
     const url = thumbnailObj?.at(0)?.url ?? ""
-    console.log(url)
-    return "https://ckc-strapi-production-33d2.up.railway.app" + url;
+    // return "https://ckc-strapi-production-33d2.up.railway.app" + url;
+    return url
 };
 
 export default extImage;
