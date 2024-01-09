c68fa0547959e9b62ef561a2980093f14c314bdf
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-01-09 12:18:57 +0530

    icons are updated

--- __config/layouts/primary.ts
+++ __config/layouts/primary.ts
@@ -1,6 +1,6 @@
 export const aside_config = [
-    { title: "Home", hrefs: ["/dashboard"], Icon: "/assets/library.png" },
-    { title: "Live", hrefs: ["/live"], Icon: "/assets/library.png" },
+    { title: "Home", hrefs: ["/dashboard"], Icon: "/assets/dashboard.png" },
+    { title: "Live", hrefs: ["/live"], Icon: "/assets/live.png" },
     { title: "Learn", hrefs: ["/learn"], Icon: "/assets/library-2.png" },
     { title: "Library", hrefs: ["/library/videos"], Icon: "/assets/library.png" },
     { title: "Challanges", hrefs: ["/challanges"], Icon: "/assets/challanges-2.png" },
