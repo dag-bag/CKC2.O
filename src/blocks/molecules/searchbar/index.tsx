d4b57979e9b8e61fcaf4a267636d8bc0041dd120
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-01-17 21:23:59 +0530

    search updated

--- src/blocks/molecules/searchbar/index.tsx
+++ src/blocks/molecules/searchbar/index.tsx
@@ -1,62 +1,74 @@
-"use client";
 import { useQueryState } from "nuqs";
 import { LuSearch } from "react-icons/lu";
 import { MultiSelect } from "@mantine/core";
 import { usePathname, useRouter } from "next/navigation";
-
-const SearchBar = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const [query, setQuery] = useQueryState("query");
-  const [grades, setGrades] = useQueryState("grades");
-  return (
-    <>
-      <div className="flex items-center border rounded-xl h-[45px] bg-white pl-3 font-heading pr-5">
-        <LuSearch size={22} color={"gray"} />
-        <input
-          placeholder="search"
-          className="border-none pt-0.5 ml-2 outline-none placeholder:capitalize bg-transparent w-[150px]"
-          onChange={async (e) => {
-            setQuery(e.target.value);
-          }}
-          value={query ?? ""}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              if (pathname !== "/search") {
-                router.push(`/search?query=${query}&grades=${grades ?? ""}`);
-              }
-            }
-          }}
-        />
-        <div className="hidden lg:block">
-          <MultiSelect
-            size="sm"
-            width={50}
-            searchable
-            data={grades1}
-            value={gradeConvertor(grades as any)}
-            onChange={(g) => {
-              setGrades(g as any);
-            }}
-            placeholder="Filter via Grades"
-          />
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default SearchBar;
+import React, { FC, ChangeEvent, KeyboardEvent } from "react";
 
 const grades1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-const gradeConvertor = (grades: string | string[] | undefined) => {
+interface SearchBarProps {}
+
+const SearchBar: FC<SearchBarProps> = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [query, setQuery] = useQueryState("query");
+  const [grades, setGrades] = useQueryState("grades");
+
+  const search = () => {
+    return router.push(`/search?query=${query}&grades=${grades ?? ""}`);
+  };
+
+  const queryOnChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && pathname !== "/search") {
+      router.push(`/search?query=${query}&grades=${grades ?? ""}`);
+    }
+  };
+
+  const gradeChangeHandler = (grade: string[]) => {
+    setGrades(grade as any);
+  };
+
+  return (
+    <div className="lg:flex hidden gap-2 items-center border rounded-full h-[50px] bg-white overflow-hidden pl-4 font-heading">
+      <input
+        value={query ?? ""}
+        placeholder="search"
+        onKeyDown={onKeyDownHandler}
+        onChange={queryOnChangeHandler}
+        className="border-none pt-0.5 ml-2 outline-none placeholder:capitalize bg-transparent w-[150px]"
+      />
+      <div>
+        <MultiSelect
+          size="sm"
+          width={50}
+          searchable
+          data={grades1}
+          onChange={gradeChangeHandler}
+          placeholder="Filter via Grades"
+          value={gradeConverter(grades as any)}
+        />
+      </div>
+      <button onClick={search} className="px-5 pl-4 bg-gray-500 h-full">
+        <LuSearch size={22} color={"white"} />
+      </button>
+    </div>
+  );
+};
+
+const gradeConverter = (grades: string | string[] = ""): string[] => {
   if (!grades) {
     return [];
   }
-  if (typeof grades == "string") {
+  if (typeof grades === "string") {
     return grades.split(",");
   } else {
     return grades;
   }
 };
+
+export default SearchBar;
