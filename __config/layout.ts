3927f7b2c952356141ac5b0b7c629ed4b7012fd1
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-01-17 16:35:09 +0530

    layout update

---
+++ __config/layout.ts
@@ -0,0 +1,50 @@
+export const backgroundGradients: any = {
+    "/dashboard": {
+        background:
+            "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
+    },
+    "/live": {
+        background:
+            "linear-gradient(to bottom, rgba(246, 235, 49, 0.89), rgba(247, 153, 30, 0.88))",
+    },
+    "/learn": {
+        background:
+            "linear-gradient(to bottom, rgba(133, 188, 61, 0.92), rgba(30, 120, 60, 0.91))",
+    },
+    "/library/videos": {
+        background:
+            "linear-gradient(to bottom, rgba(133, 188, 61, 0.92), rgba(30, 120, 60, 0.91))",
+    },
+
+    "/library/comics": {
+        background:
+            "linear-gradient(to bottom, rgba(246, 235, 49, 0.89), rgba(247, 153, 30, 0.88))",
+    },
+    "/challanges": {
+        background:
+            "linear-gradient(to bottom, rgba(133, 188, 61, 0.92), rgba(30, 120, 60, 0.91))",
+    },
+    "/discovery-bag": {
+        background:
+            "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
+    },
+
+    "/leader": {
+        background:
+            "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
+    },
+
+    "/shop/virtual": {
+        background:
+            "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
+    },
+    "/nac": {
+        background:
+            "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
+    },
+
+    "/search": {
+        background:
+            "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
+    },
+};
--- src/blocks/layouts/primary/index.tsx
+++ src/blocks/layouts/primary/index.tsx
@@ -8,83 +8,30 @@ import Aside from "./aside";
 import Header from "./header";
 import React, { ReactNode } from "react";
 import { usePathname } from "next/navigation";
-import UpgadePlanAlert from "@/blocks/popups/upgrade-plan-alert";
+import { backgroundGradients } from "@config/layout";
 
-const theme: any = {
-  "/dashboard": {
-    background:
-      "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
-  },
-  "/live": {
-    background:
-      "linear-gradient(to bottom, rgba(246, 235, 49, 0.89), rgba(247, 153, 30, 0.88))",
-  },
-  "/learn": {
-    background:
-      "linear-gradient(to bottom, rgba(133, 188, 61, 0.92), rgba(30, 120, 60, 0.91))",
-  },
-  "/library/videos": {
-    background:
-      "linear-gradient(to bottom, rgba(133, 188, 61, 0.92), rgba(30, 120, 60, 0.91))",
-  },
-
-  "/library/comics": {
-    background:
-      "linear-gradient(to bottom, rgba(246, 235, 49, 0.89), rgba(247, 153, 30, 0.88))",
-  },
-  "/challanges": {
-    background:
-      "linear-gradient(to bottom, rgba(133, 188, 61, 0.92), rgba(30, 120, 60, 0.91))",
-  },
-  "/discovery-bag": {
-    background:
-      "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
-  },
-
-  "/leader": {
-    background:
-      "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
-  },
-
-  "/shop/virtual": {
-    background:
-      "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
-  },
-  "/nac": {
-    background:
-      "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
-  },
-
-  "/search": {
-    background:
-      "linear-gradient(to bottom, rgba(0, 174, 239, 0.9), rgba(34, 46, 120, 0.92))",
-  },
+const wrapperStyle = {
+  backgroundSize: "800px 800px",
+  backgroundImage: "url('/pattern.jpg')",
 };
 
 const PrimaryLayout: React.FC<Props> = ({ children }) => {
   const pathname = usePathname();
 
+  const mainStyle = backgroundGradients[pathname] ?? {
+    background: "whitesmoke",
+  };
+
   return (
-    <div
-      className="h-screen font-fun"
-      style={{
-        backgroundSize: "800px 800px",
-        backgroundImage: "url('/pattern.jpg')",
-      }}
-    >
+    <div className="h-screen font-fun" style={wrapperStyle}>
       <main className="grid md:grid-cols-[120px_auto] h-screen">
         <Aside />
         <div
-          style={
-            theme[pathname] ?? {
-              background: "whitesmoke",
-            }
-          }
-          className="max-w-screen w-full overflow-y-scroll hide-scrollbar relative pb-5"
+          style={mainStyle}
+          className="max-w-screen w-full overflow-y-scroll relative pb-5"
         >
           <Header />
           <div className="pt-[100px] max-w-[1440px] mx-auto px-5">
-            <UpgadePlanAlert />
             {children}
           </div>
         </div>
