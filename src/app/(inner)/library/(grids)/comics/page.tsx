c72573a32db705c0f94aa444da5e9335982e4d29
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-01-17 22:14:19 +0530

    responsive library

--- src/app/(inner)/library/(grids)/comics/page.tsx
+++ src/app/(inner)/library/(grids)/comics/page.tsx
@@ -10,7 +10,7 @@ export default async function page() {
   ]);
   const listOfPurchagesIds = purchases?.map((pur) => pur.content_id);
   return (
-    <section className="grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 gap-x-4 gap-y-6">
+    <section className="grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 md:gap-5 gap-3">
       {data.map((video: any) => {
         return (
           <ContentCard
--- src/app/(inner)/library/(grids)/videos/page.tsx
+++ src/app/(inner)/library/(grids)/videos/page.tsx
@@ -1,7 +1,7 @@
+import extImage from "@/libs/extImage";
 import { Videos } from "@/strapi/services/api";
 import { getTransactions } from "@/strapi/services/me";
 import ContentCard from "@/blocks/molecules/content-card";
-import extImage from "@/libs/extImage";
 const VideosPage = async () => {
   const [data, purchases] = await Promise.all([
     Videos({ type: "GET" }),
@@ -11,26 +11,24 @@ const VideosPage = async () => {
   const listOfPurchagesIds = purchases?.map((pur) => pur.content_id);
   return (
     <>
-      <section className="grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-1 md:gap-x-4 gap-y-6">
+      <section className="grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 md:gap-5 gap-3">
         {data.map((video: any) => (
-          <>
-            <ContentCard
-              key={video.id}
-              {...{
-                id: video.id,
-                type: "video",
-                theme: "green",
-                slug: video.slug,
-                desc: video.desc,
-                title: video.title,
-                price: video.price,
-                grades: video.grade,
-                isPremium: video.premium,
-                thumbnail: extImage(video.thumbnail),
-                isUnlocked: listOfPurchagesIds?.includes(`${video.id}`),
-              }}
-            />
-          </>
+          <ContentCard
+            key={video.id}
+            {...{
+              id: video.id,
+              type: "video",
+              theme: "green",
+              slug: video.slug,
+              desc: video.desc,
+              title: video.title,
+              price: video.price,
+              grades: video.grade,
+              isPremium: video.premium,
+              thumbnail: extImage(video.thumbnail),
+              isUnlocked: listOfPurchagesIds?.includes(`${video.id}`),
+            }}
+          />
         ))}
       </section>
     </>
